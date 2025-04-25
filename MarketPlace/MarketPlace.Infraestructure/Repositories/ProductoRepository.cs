e5c4ff2080b84858e81c971c3d99da162b619069
Keison28 <felucho.cerda@gmail.com>
2025-04-25 00:33:57 -0400

    Producto Repository

---
+++ MarketPlace/MarketPlace.Infraestructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,49 @@
+﻿// En Marketplace.Infrastructure
+using Marketplace.Domain.Entities;
+using Marketplace.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.Infrastructure.Repositories
+{
+    public class ProductoRepository
+    {
+        private readonly DataContext _context;
+
+        public ProductoRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Producto>> GetAllAsync()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+
+        public async Task<Producto> GetByIdAsync(int id)
+        {
+            return await _context.Productos.FindAsync(id);
+        }
+
+        public async Task AddAsync(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Producto producto)
+        {
+            _context.Productos.Update(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
